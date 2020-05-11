325aa566641fd69371a3d126d49a12be467279bc
ABDULAZIZ94 <abdulazizabdulnoor@gmail.com>
2020-05-11 18:23:00 +0800

    add __part1 and __part2

--- views/site/about.php
+++ views/site/about.php
@@ -1,18 +1,15 @@
 <?php
-
-/* @var $this yii\web\View */
-
-use yii\helpers\Html;
-
-$this->title = 'About';
-$this->params['breadcrumbs'][] = $this->title;
+   /* @var $this yii\web\View */
+   use yii\helpers\Html;
+   $this->title = 'About';
+   $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>
-        This is the About page. You may modify the following file to customize its content:
-    </p>
-
-    <code><?= __FILE__ ?></code>
+   <h1><?= Html::encode($this->title) ?></h1>
+   <p>
+      This is the About page. You may modify the following file to customize its content:
+   </p>
+   <?= $this->render("__part1") ?>
+   <?= $this->render("__part2") ?>
+   <code><?= __FILE__ ?></code>
 </div>
