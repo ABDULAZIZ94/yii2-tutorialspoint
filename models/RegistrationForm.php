271ffb1c2e286876b33543298de0214a76cdb2cd
ABDULAZIZ94 <abdulazizabdulnoor@gmail.com>
2020-05-12 20:21:33 +0800

    step 7: custom error message ok

--- models/RegistrationForm.php
+++ models/RegistrationForm.php
@@ -11,9 +11,9 @@
       public $phone;
       public function rules() {
          return [
-            // the username, password, email, country, city, and phone attributes are
-            //required
-            [['username' ,'password', 'email', 'country', 'city', 'phone'], 'required'],
+            // the username, password, email, country, city, and phone attributes are required
+            [['password', 'email', 'country', 'city', 'phone'], 'required'],
+            ['username', 'required', 'message' => 'Username is required'],
             // the email attribute should be a valid email address
             ['email', 'email'],
          ];
