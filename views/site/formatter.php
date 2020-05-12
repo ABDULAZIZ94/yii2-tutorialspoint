0bb221dcb040f518a19f8cc0a1977a5aa437f2f1
ABDULAZIZ94 <abdulazizabdulnoor@gmail.com>
2020-05-13 05:58:29 +0800

    Number Format step 2 ok

--- views/site/formatter.php
+++ views/site/formatter.php
@@ -1,7 +1,10 @@
 <?php
    $formatter = \Yii::$app->formatter;
-   echo $formatter->asDate(date('Y-m-d'), 'short'),"<br>";
-   echo $formatter->asDate(date('Y-m-d'), 'medium'),"<br>";
-   echo $formatter->asDate(date('Y-m-d'), 'long'),"<br>";
-   echo $formatter->asDate(date('Y-m-d'), 'full'),"<br>";
+   echo Yii::$app->formatter->asInteger(105),"<br>";
+   echo Yii::$app->formatter->asDecimal(105.41),"<br>";
+   echo Yii::$app->formatter->asPercent(0.51),"<br>";
+   echo Yii::$app->formatter->asScientific(105),"<br>";
+   echo Yii::$app->formatter->asCurrency(105),"<br>";
+   echo Yii::$app->formatter->asSize(105),"<br>";
+   echo Yii::$app->formatter->asShortSize(105),"<br>";
 ?>
