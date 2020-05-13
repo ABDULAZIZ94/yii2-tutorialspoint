1cfcfdecaa26c358f0ceb8ef50fffa92b2a2871d
ABDULAZIZ94 <abdulazizabdulnoor@gmail.com>
2020-05-13 13:02:58 +0800

    step 4: ok

--- views/site/datawidget.php
+++ views/site/datawidget.php
@@ -2,5 +2,15 @@
    use yii\grid\GridView;
    echo GridView::widget([
       'dataProvider' => $dataProvider,
+      'columns' => [
+         'id',
+         [
+            'class' => 'yii\grid\DataColumn', // can be omitted, as it is the default
+            'label' => 'Name and email',
+            'value' => function ($data) {
+               return $data->name . " writes from " . $data->email;
+            },
+         ],
+      ],
    ]);
 ?>
