256b938008e5af046acef0058e749e4b91cbc36c
ABDULAZIZ94 <abdulazizabdulnoor@gmail.com>
2020-05-13 13:04:16 +0800

    step 6: ok

--- views/site/datawidget.php
+++ views/site/datawidget.php
@@ -3,14 +3,9 @@
    echo GridView::widget([
       'dataProvider' => $dataProvider,
       'columns' => [
-         'id',
-         [
-            'class' => 'yii\grid\DataColumn', // can be omitted, as it is the default
-            'label' => 'Name and email',
-            'value' => function ($data) {
-               return $data->name . " writes from " . $data->email;
-            },
-         ],
+         ['class' => 'yii\grid\SerialColumn'], 'name',
+         ['class' => 'yii\grid\ActionColumn'],
+         ['class' => 'yii\grid\CheckboxColumn'],
       ],
    ]);
 ?>
